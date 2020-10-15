6d3893e8a77ecae523dcdd929e052f2bf91311d6
Ashley Morton-Bye <ashley.morton.bye@gmail.com>
2020-10-15 12:58:41 +0100

    Implement interpreter for nested addition, subtraction, multiplication and division

--- securities.py
+++ securities.py
@@ -2,6 +2,8 @@
 from __future__ import annotations
 
 import csv
+import dataclasses
+import json
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import Dict, List
@@ -39,6 +41,47 @@ class Security:
     shares: float
 
 
+class Interpreter:
+    def __init__(self, securities: Dict[str, Security]) -> None:
+        self._securities = securities
+
+    def execute(self, query: Dict) -> float:
+        security = self._securities[query['security']]
+        expression_tree = self._parse(query['expression'], security)
+        return self._evaluate(expression_tree)
+
+    def _parse(self, expression: Dict, security: Security):
+        return {
+            'operator': expression['fn'],
+            'a': self._evaluate_operand(expression['a'], security),
+            'b': self._evaluate_operand(expression['b'], security),
+        }
+
+    def _evaluate_operand(self, exp, security):
+        if type(exp) == dict:
+            return self._parse(exp, security)
+        elif type(exp) == float or type(exp) == int:
+            return float(exp)
+        else:
+            return dataclasses.asdict(security)[exp]
+
+    def _evaluate(self, expression):
+        if type(expression) == float:
+            return expression
+
+        operations = {
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: a / b,
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+        }
+
+        return operations[expression['operator']](
+            self._evaluate(expression['a']),
+            self._evaluate(expression['b'])
+        )
+
+
 def load_csv(path: str) -> List[Dict]:
     with open(path, 'r') as f:
         in_file = csv.DictReader(f)
@@ -51,8 +94,19 @@ def main():
     raw_attributes = load_csv('attributes.csv')
     raw_facts = load_csv('facts.csv')
 
+    query = """{
+        "expression": {
+            "fn": "-", 
+            "a": {"fn": "-", "a": "eps", "b": "shares"}, 
+            "b": {"fn": "-", "a": "assets", "b": "liabilities"}
+        },
+        "security": "CDE"
+    }"""
+
     securities = build_securities(raw_securities, raw_attributes, raw_facts)
-    print(securities)
+    interpreter = Interpreter(securities)
+    result = interpreter.execute(json.loads(query))
+    print(result)
 
 
 if __name__ == '__main__':
--- test_securities.py
+++ test_securities.py
@@ -1,41 +1,44 @@
+import json
+
 import securities
+
+RAW_SECURITIES = [
+    {'id': '1', 'symbol': 'ABC'}, {'id': '2', 'symbol': 'BCD'},
+]
+RAW_ATTRIBUTES = [
+    {'id': '1', 'name': 'price'}, {'id': '2', 'name': 'eps'},
+    {'id': '3', 'name': 'dps'}, {'id': '4', 'name': 'sales'},
+    {'id': '5', 'name': 'ebitda'}, {'id': '6', 'name': 'free_cash_flow'},
+    {'id': '7', 'name': 'assets'}, {'id': '8', 'name': 'liabilities'},
+    {'id': '9', 'name': 'debt'}, {'id': '10', 'name': 'shares'},
+]
+RAW_FACTS = [
+    {'security_id': '1', 'attribute_id': '1', 'value': '1.0'},
+    {'security_id': '1', 'attribute_id': '2', 'value': '2.0'},
+    {'security_id': '1', 'attribute_id': '3', 'value': '3.0'},
+    {'security_id': '1', 'attribute_id': '4', 'value': '4.0'},
+    {'security_id': '1', 'attribute_id': '5', 'value': '5.0'},
+    {'security_id': '1', 'attribute_id': '6', 'value': '6.0'},
+    {'security_id': '1', 'attribute_id': '7', 'value': '7.0'},
+    {'security_id': '1', 'attribute_id': '8', 'value': '8.0'},
+    {'security_id': '1', 'attribute_id': '9', 'value': '9.0'},
+    {'security_id': '1', 'attribute_id': '10', 'value': '10.0'},
+    {'security_id': '2', 'attribute_id': '1', 'value': '2.0'},
+    {'security_id': '2', 'attribute_id': '2', 'value': '4.0'},
+    {'security_id': '2', 'attribute_id': '3', 'value': '6.0'},
+    {'security_id': '2', 'attribute_id': '4', 'value': '8.0'},
+    {'security_id': '2', 'attribute_id': '5', 'value': '10.0'},
+    {'security_id': '2', 'attribute_id': '6', 'value': '12.0'},
+    {'security_id': '2', 'attribute_id': '7', 'value': '14.0'},
+    {'security_id': '2', 'attribute_id': '8', 'value': '16.0'},
+    {'security_id': '2', 'attribute_id': '9', 'value': '18.0'},
+    {'security_id': '2', 'attribute_id': '10', 'value': '20.0'},
+]
 
 
 def test_builds_securities_from_raw_data():
-    raw_securities = [
-        {'id': '1', 'symbol': 'ABC'}, {'id': '2', 'symbol': 'BCD'}
-    ]
-    raw_attributes = [
-        {'id': '1', 'name': 'price'}, {'id': '2', 'name': 'eps'},
-        {'id': '3', 'name': 'dps'}, {'id': '4', 'name': 'sales'},
-        {'id': '5', 'name': 'ebitda'}, {'id': '6', 'name': 'free_cash_flow'},
-        {'id': '7', 'name': 'assets'}, {'id': '8', 'name': 'liabilities'},
-        {'id': '9', 'name': 'debt'}, {'id': '10', 'name': 'shares'}
-    ]
-    raw_facts = [
-        {'security_id': '1', 'attribute_id': '1', 'value': '1.0'},
-        {'security_id': '1', 'attribute_id': '2', 'value': '2.0'},
-        {'security_id': '1', 'attribute_id': '3', 'value': '3.0'},
-        {'security_id': '1', 'attribute_id': '4', 'value': '4.0'},
-        {'security_id': '1', 'attribute_id': '5', 'value': '5.0'},
-        {'security_id': '1', 'attribute_id': '6', 'value': '6.0'},
-        {'security_id': '1', 'attribute_id': '7', 'value': '7.0'},
-        {'security_id': '1', 'attribute_id': '8', 'value': '8.0'},
-        {'security_id': '1', 'attribute_id': '9', 'value': '9.0'},
-        {'security_id': '1', 'attribute_id': '10', 'value': '10.0'},
-        {'security_id': '2', 'attribute_id': '1', 'value': '2.0'},
-        {'security_id': '2', 'attribute_id': '2', 'value': '4.0'},
-        {'security_id': '2', 'attribute_id': '3', 'value': '6.0'},
-        {'security_id': '2', 'attribute_id': '4', 'value': '8.0'},
-        {'security_id': '2', 'attribute_id': '5', 'value': '10.0'},
-        {'security_id': '2', 'attribute_id': '6', 'value': '12.0'},
-        {'security_id': '2', 'attribute_id': '7', 'value': '14.0'},
-        {'security_id': '2', 'attribute_id': '8', 'value': '16.0'},
-        {'security_id': '2', 'attribute_id': '9', 'value': '18.0'},
-        {'security_id': '2', 'attribute_id': '10', 'value': '20.0'}
-    ]
+    s = securities.build_securities(RAW_SECURITIES, RAW_ATTRIBUTES, RAW_FACTS)
 
-    s = securities.build_securities(raw_securities, raw_attributes, raw_facts)
     assert len(s) == 2
 
     abc = s['ABC']
@@ -62,3 +65,93 @@ def test_builds_securities_from_raw_data():
     assert bcd.liabilities == 16.0
     assert bcd.debt == 18.0
     assert bcd.shares == 20.0
+
+
+def test_multiplication_query():
+    query = """{
+        "expression": {"fn": "*", "a": "sales", "b": 2},
+        "security": "ABC"
+    }"""
+
+    s = securities.build_securities(RAW_SECURITIES, RAW_ATTRIBUTES, RAW_FACTS)
+
+    interpreter = securities.Interpreter(s)
+    result = interpreter.execute(json.loads(query))
+
+    assert result == 8.0
+
+
+def test_division_query():
+    query = """{
+        "expression": {"fn": "/", "a": "price", "b": "eps"},
+        "security": "BCD"
+    }"""
+
+    s = securities.build_securities(RAW_SECURITIES, RAW_ATTRIBUTES, RAW_FACTS)
+
+    interpreter = securities.Interpreter(s)
+    result = interpreter.execute(json.loads(query))
+
+    assert result == 0.5
+
+
+def test_addition_query():
+    query = """{
+        "expression": {"fn": "+", "a": "liabilities", "b": "debt"},
+        "security": "ABC"
+    }"""
+
+    s = securities.build_securities(RAW_SECURITIES, RAW_ATTRIBUTES, RAW_FACTS)
+
+    interpreter = securities.Interpreter(s)
+    result = interpreter.execute(json.loads(query))
+
+    assert result == 17.0
+
+
+def test_subtraction_query():
+    query = """{
+        "expression": {"fn": "-", "a": "sales", "b": "debt"},
+        "security": "BCD"
+    }"""
+
+    s = securities.build_securities(RAW_SECURITIES, RAW_ATTRIBUTES, RAW_FACTS)
+
+    interpreter = securities.Interpreter(s)
+    result = interpreter.execute(json.loads(query))
+
+    assert result == -10.0
+
+
+def test_nested_expressions_query():
+    raw_securities = [
+        {'id': '3', 'symbol': 'CDE'},
+    ]
+    raw_facts = [
+        {'security_id': '3', 'attribute_id': '1', 'value': '3.0'},
+        {'security_id': '3', 'attribute_id': '2', 'value': '6.0'},
+        {'security_id': '3', 'attribute_id': '3', 'value': '9.0'},
+        {'security_id': '3', 'attribute_id': '4', 'value': '12.0'},
+        {'security_id': '3', 'attribute_id': '5', 'value': '15.0'},
+        {'security_id': '3', 'attribute_id': '6', 'value': '18.0'},
+        {'security_id': '3', 'attribute_id': '7', 'value': '21.0'},
+        {'security_id': '3', 'attribute_id': '8', 'value': '24.0'},
+        {'security_id': '3', 'attribute_id': '9', 'value': '27.0'},
+        {'security_id': '3', 'attribute_id': '10', 'value': '30.0'},
+    ]
+
+    query = """{
+        "expression": {
+            "fn": "-", 
+            "a": {"fn": "-", "a": "eps", "b": "shares"}, 
+            "b": {"fn": "-", "a": "assets", "b": "liabilities"}
+        },
+        "security": "CDE"
+    }"""
+
+    s = securities.build_securities(raw_securities, RAW_ATTRIBUTES, raw_facts)
+
+    interpreter = securities.Interpreter(s)
+    result = interpreter.execute(json.loads(query))
+
+    assert result == -21.0
