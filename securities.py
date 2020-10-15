47754f5a743d9640874a3c3ba35bf80c5938a78a
Ashley Morton-Bye <ashley.morton.bye@gmail.com>
2020-10-15 08:38:56 +0100

    Load securities data from CSV

--- securities.py
+++ securities.py
@@ -1,11 +1,58 @@
 #!/usr/bin/env python
+from __future__ import annotations
 
-def hello():
-    return 'Hello, Securities!'
+import csv
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Dict, List
+
+
+def build_securities(
+        raw_securities: List[Dict],
+        raw_attributes: List[Dict],
+        raw_facts: List[Dict]
+) -> Dict[str, Security]:
+    securities = {security['id']: security['symbol'] for security in raw_securities}
+    attributes = {attr['id']: attr['name'] for attr in raw_attributes}
+    facts = defaultdict(dict)
+    for fact in raw_facts:
+        security_id = securities[fact['security_id']]
+        attribute_id = attributes[fact['attribute_id']]
+        value = float(fact['value'])
+        facts[security_id][attribute_id] = value
+
+    return {symbol: Security(symbol, **attrs) for symbol, attrs in facts.items()}
+
+
+@dataclass(frozen=True)
+class Security:
+    symbol: str
+    price: float
+    eps: float
+    dps: float
+    sales: float
+    ebitda: float
+    free_cash_flow: float
+    assets: float
+    liabilities: float
+    debt: float
+    shares: float
+
+
+def load_csv(path: str) -> List[Dict]:
+    with open(path, 'r') as f:
+        in_file = csv.DictReader(f)
+        lines = [row for row in in_file]
+    return lines
 
 
 def main():
-    print(hello())
+    raw_securities = load_csv('securities.csv')
+    raw_attributes = load_csv('attributes.csv')
+    raw_facts = load_csv('facts.csv')
+
+    securities = build_securities(raw_securities, raw_attributes, raw_facts)
+    print(securities)
 
 
 if __name__ == '__main__':
--- test_securities.py
+++ test_securities.py
@@ -1,5 +1,64 @@
 import securities
 
 
-def test_hello_securities():
-    assert securities.hello() == 'Hello, Securities!'
+def test_builds_securities_from_raw_data():
+    raw_securities = [
+        {'id': '1', 'symbol': 'ABC'}, {'id': '2', 'symbol': 'BCD'}
+    ]
+    raw_attributes = [
+        {'id': '1', 'name': 'price'}, {'id': '2', 'name': 'eps'},
+        {'id': '3', 'name': 'dps'}, {'id': '4', 'name': 'sales'},
+        {'id': '5', 'name': 'ebitda'}, {'id': '6', 'name': 'free_cash_flow'},
+        {'id': '7', 'name': 'assets'}, {'id': '8', 'name': 'liabilities'},
+        {'id': '9', 'name': 'debt'}, {'id': '10', 'name': 'shares'}
+    ]
+    raw_facts = [
+        {'security_id': '1', 'attribute_id': '1', 'value': '1.0'},
+        {'security_id': '1', 'attribute_id': '2', 'value': '2.0'},
+        {'security_id': '1', 'attribute_id': '3', 'value': '3.0'},
+        {'security_id': '1', 'attribute_id': '4', 'value': '4.0'},
+        {'security_id': '1', 'attribute_id': '5', 'value': '5.0'},
+        {'security_id': '1', 'attribute_id': '6', 'value': '6.0'},
+        {'security_id': '1', 'attribute_id': '7', 'value': '7.0'},
+        {'security_id': '1', 'attribute_id': '8', 'value': '8.0'},
+        {'security_id': '1', 'attribute_id': '9', 'value': '9.0'},
+        {'security_id': '1', 'attribute_id': '10', 'value': '10.0'},
+        {'security_id': '2', 'attribute_id': '1', 'value': '2.0'},
+        {'security_id': '2', 'attribute_id': '2', 'value': '4.0'},
+        {'security_id': '2', 'attribute_id': '3', 'value': '6.0'},
+        {'security_id': '2', 'attribute_id': '4', 'value': '8.0'},
+        {'security_id': '2', 'attribute_id': '5', 'value': '10.0'},
+        {'security_id': '2', 'attribute_id': '6', 'value': '12.0'},
+        {'security_id': '2', 'attribute_id': '7', 'value': '14.0'},
+        {'security_id': '2', 'attribute_id': '8', 'value': '16.0'},
+        {'security_id': '2', 'attribute_id': '9', 'value': '18.0'},
+        {'security_id': '2', 'attribute_id': '10', 'value': '20.0'}
+    ]
+
+    s = securities.build_securities(raw_securities, raw_attributes, raw_facts)
+    assert len(s) == 2
+
+    abc = s['ABC']
+    bcd = s['BCD']
+
+    assert abc.price == 1.0
+    assert abc.eps == 2.0
+    assert abc.dps == 3.0
+    assert abc.sales == 4.0
+    assert abc.ebitda == 5.0
+    assert abc.free_cash_flow == 6.0
+    assert abc.assets == 7.0
+    assert abc.liabilities == 8.0
+    assert abc.debt == 9.0
+    assert abc.shares == 10.0
+
+    assert bcd.price == 2.0
+    assert bcd.eps == 4.0
+    assert bcd.dps == 6.0
+    assert bcd.sales == 8.0
+    assert bcd.ebitda == 10.0
+    assert bcd.free_cash_flow == 12.0
+    assert bcd.assets == 14.0
+    assert bcd.liabilities == 16.0
+    assert bcd.debt == 18.0
+    assert bcd.shares == 20.0
